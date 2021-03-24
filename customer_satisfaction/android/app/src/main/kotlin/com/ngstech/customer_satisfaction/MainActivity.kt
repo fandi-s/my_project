2680b5fcc7b77c294e979286e90ca78280f991f7
fandi-s <81291456+fandi-s@users.noreply.github.com>
2021-03-24 16:58:59 +0700

    k_const

--- customer_satisfaction/android/app/src/main/kotlin/com/ngstech/customer_satisfaction/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.ngstech.customer_satisfaction
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- daily_monitoring/android/app/src/main/kotlin/com/ngs_tech/daily_monitoring/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.ngs_tech.daily_monitoring
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
